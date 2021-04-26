3ec3a58feb71534a312c743d17b778f9526866d1
Antonio <Antonio@AntonioPC>
2021-04-26 08:00:35 +0200

    Soluzione con modifiche al ConnectDB

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/DAO/ConnectDB.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/DAO/ConnectDB.java
@@ -1,18 +1,28 @@
 package it.polito.tdp.anagrammi.DAO;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.Connection; 
 import java.sql.SQLException;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectDB {
 	
 	// check user e password
 	static private final String jdbcUrl = "jdbc:mysql://localhost/dizionario?user=root&password=root";
 
+	static private HikariDataSource ds = null;
+	
 	public static Connection getConnection() {
+		
+		if(ds == null) {
+			ds = new HikariDataSource();
+			
+			ds.setJdbcUrl(jdbcUrl);
+//			ds.setUsername("root");
+//			ds.setPassword("root");
+		}
 
 		try {
-				Connection connection = DriverManager.getConnection(jdbcUrl);
+				Connection connection = ds.getConnection();
 				return connection;
 
 		} catch (SQLException e) {
